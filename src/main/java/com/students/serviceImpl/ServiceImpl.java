351ce999be89a9e574c496494e4b61b84a5e2a44
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-04-09 15:35:12 +0530

    update

--- src/main/java/com/students/serviceImpl/ServiceImpl.java
+++ src/main/java/com/students/serviceImpl/ServiceImpl.java
@@ -3,11 +3,13 @@ package com.students.serviceImpl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.students.entity.Student;
 import com.students.repository.Studentrepositary;
 import com.students.service.StudentService;
 
+@Service
 public class ServiceImpl implements StudentService {
 
 	@Autowired
